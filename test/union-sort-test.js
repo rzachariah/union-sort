305a44ae25258415a2766367cfaeb725b0ccc4cd
Ranjith Zachariah <rzachariah@ezesoft.com>
2019-05-20 22:59:08 -0400

    More testcases

--- test/union-sort-test.js
+++ test/union-sort-test.js
@@ -4,27 +4,33 @@ var expect = chai.expect;
 const unionSort = require('../lib/union-sort');
 describe('unionSort', function() {
     it('should return empty list when input lists are empty', function() {
-        expect(_.isEqual(unionSort([],[]), [])).to.be.true;
+        expect(unionSort([],[])).to.eql([]);
     });
     it('should return first list when second list is empty', function() {
-        expect(_.isEqual(unionSort([1,2,3],[]), [1,2,3])).to.be.true;
+        expect(unionSort([1,2,3],[])).to.eql([1,2,3]);
     });
     it('should return second list when first list is empty', function() {
-        expect(_.isEqual(unionSort([],[1,2,3]), [1,2,3])).to.be.true;
+        expect(unionSort([],[1,2,3])).to.eql([1,2,3]);
     });
     it('should return first list when second list is identical', function() {
-        expect(_.isEqual(unionSort([1,2,3],[1,2,3]), [1,2,3])).to.be.true;
+        expect(unionSort([1,2,3],[1,2,3])).to.eql([1,2,3]);
     });
     it('should return sorted union when first list is strictly less than second list', function() {
-        expect(_.isEqual(unionSort([1,2,3],[4,5,6]), [1,2,3,4,5,6])).to.be.true;
+        expect(unionSort([1,2,3],[4,5,6])).to.eql([1,2,3,4,5,6]);
     });
     it('should return sorted union when second list is strictly less than first list', function() {
-        expect(_.isEqual(unionSort([4,5,6],[1,2,3]), [1,2,3,4,5,6])).to.be.true;
+        expect(unionSort([4,5,6],[1,2,3])).to.eql([1,2,3,4,5,6]);
     });
-    it('should return sorted union of two nonempty sorted lists', function() {
-        expect(_.isEqual(unionSort([1,3,5],[2,4,6]), [1,2,3,4,5,6])).to.be.true;
+    it('should return sorted union of two nonempty distinct sorted lists', function() {
+        expect(unionSort([1,3,5],[2,4,6])).to.eql([1,2,3,4,5,6]);
     });
-    it('should return sorted union without duplicates', function() {
-        expect(_.isEqual(unionSort([1,3,5],[1,2,3,4,5,6]), [1,2,3,4,5,6])).to.be.true;
+    it('should return sorted union without duplicates when first input contains duplicates', function() {
+        expect(unionSort([1,3,5,5,5],[2,4,6])).to.eql([1,2,3,4,5,6]);
+    });
+    it('should return sorted union without duplicates when second input contains duplicates', function() {
+        expect(unionSort([1,3,5],[2,4,6,6,6])).to.eql([1,2,3,4,5,6]);
+    });
+    it('should return sorted union without duplicates on partially overlapping input', function() {
+        expect(unionSort([1,3,5],[2,4,5,6,8,10])).to.eql([1,2,3,4,5,6,8,10]);
     });
 });
